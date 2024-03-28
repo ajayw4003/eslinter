37b08bb3ee42fe94032d73b500dd960655a316a6
ajayw4003 <ajayw4003@gmail.com>
2024-03-28 14:37:37 +0530

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  return <div className="App">Hellodd</div>;
+  return <div className="App">Hellow</div>;
 }
 
 export default App;
